814ef4022e2d8f3a1872cf7899b3dbc2dba58dd8
Mohdsohail7 <sohailmohd437@gmail.com>
2024-09-18 22:50:36 +0530

    MS1.2: code added

--- trip_planner/index.js
+++ trip_planner/index.js
@@ -1,3 +1,28 @@
 require("dotenv").config();
+const axiosInstance = require("./lib/axios");
 
-console.log(process.env.CLIENT_KEY);
+axiosInstance.get("/health")
+.then((response) => console.log(response.data))
+.catch((error) => console.log("Error fetching axios error", error));
+
+
+// Implement the getFlights function. This function takes in two parameters: the origin and the destination.
+const getFlights = async(origin, destination) => {
+    try {
+        const response = await axiosInstance.get('/flights/search', {
+            params: {
+                origin: origin,
+                destination: destination
+            },
+        });
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+getFlights("bengaluru", "dehradun")
+.then((flights) => console.log("Flights data-->", flights))
+.catch((error) => {
+    console.log(error);
+});
