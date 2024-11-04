5e061151a879e12adbf61b2b0c5c12bf3ae35ed5
Mohdsohail7 <sohailmohd437@gmail.com>
2024-11-04 22:07:42 +0530

    Resolved merge conflict in .gitignore

--- concert_planning/index.js
+++ concert_planning/index.js
@@ -1,4 +1,24 @@
 require("dotenv").config();
+const axiosInstance = require('./lib/axios');
 
-console.log(process.env.CLIENT_KEY);
-console.log(process.env.CLIENT_SECRET);
+axiosInstance.get('/health')
+.then((response) => console.log(response.data))
+.catch((error) => console.log('Error fetching axios error', error));
+
+const getConcertsByArtistAndCity = async(artist, city) => {
+    try {
+        const response = await axiosInstance.get('/concerts/search', {
+            params: {
+                artist: artist,
+                city: city
+            },
+        });
+        return response.data ;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getConcertsByArtistAndCity('Taylor Swift', 'Las Vegas')
+.then((data) => console.log(data))
+.catch((error) => console.log(error));
