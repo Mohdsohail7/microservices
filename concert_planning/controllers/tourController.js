9bd7eb9db432d8536eb1121801180979239648d9
Mohdsohail7 <sohailmohd437@gmail.com>
2024-11-06 10:45:06 +0530

    Rate Limit Error: I added rate limit error in my  code

--- concert_planning/controllers/tourController.js
+++ concert_planning/controllers/tourController.js
@@ -2,7 +2,10 @@ const axiosInstance = require("../lib/axios");
 
 const getConcerts = async(req, res) => {
     try {
-        const response = await axiosInstance.get("/concerts", {
+        const test_error = req.query.test_error;
+        const rate_limit = req.query.rate_limit;
+
+        const response = await axiosInstance.get(`/concerts?test_error=${test_error}&rate_limit=${rate_limit}`, {
             headers: {
                 CLIENT_KEY: process.env.CLIENT_KEY,
                 CLIENT_SECRET: process.env.CLIENT_SECRET
@@ -14,29 +17,58 @@ const getConcerts = async(req, res) => {
     } catch (error) {
         console.error(error);
 
-        res.status(500).json({ error: "failed to fetch concerts"});
+        if (error.response.status === 429) {
+            return res.status(429).json({ error: "Rate limit exceeded. Please try again later"});
+        } else if (error.response.status === 500 && error.response.data.error === "Simulated error for testing purposes.") {
+            return res.status(500).json({ error: "Simulated error for testing purposes."});
+        } else {
+            res.status(500).json({ error: "failed to fetch concerts"});
+        }
+        
     }
 }
 
 const getMerchandiseStalls = async(req, res) => {
     try {
-        const response = await axiosInstance.get("/merchandiseStalls");
+        const test_error = req.query.test_error;
+        const rate_limit = req.query.rate_limit;
+
+        const response = await axiosInstance.get(`/merchandiseStalls?test_error=${test_error}&rate_limit=${rate_limit}`);
         res.json(response.data);
 
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: "Failed to fetch merchandiseStalls"});
+
+        if (error.response.status === 429) {
+            return res.status(429).json({ error: "Rate limit exceeded. Please try again later"});
+        } else if (error.response.status === 500 && error.response.data.error === "Simulated error for testing purposes.") {
+            return res.status(500).json({ error: "Simulated error for testing purposes."});
+        } else {
+            res.status(500).json({ error: "failed to fetch merchandiseStalls"});
+        }
+        
     }
 }
 
 const getAfterParties = async(req, res) => {
     try {
-        const response = await axiosInstance.get("/afterParties");
+        const test_error = req.query.test_error;
+        const rate_limit = req.query.rate_limit;
+
+        const response = await axiosInstance.get(`/afterParties?test_error=${test_error}&rate_limit=${rate_limit}`);
         res.json(response.data);
 
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: "failed to fetch afterParty"});
+
+         if (error.response.status === 429) {
+            return res.status(429).json({ error: "Rate limit exceeded. Please try again later"});
+        } else if (error.response.status === 500 && error.response.data.error === "Simulated error for testing purposes.") {
+            return res.status(500).json({ error: "Simulated error for testing purposes."});
+        } else {
+            res.status(500).json({ error: "failed to fetch afterParties"});
+        }
+        
     }
 }
 
